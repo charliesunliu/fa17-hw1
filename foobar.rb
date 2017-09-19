4fbba3ef1c41afa36e3441e493bb403ab60a7b39
charliesunliu <cccharliesunliu@gmail.com>
2017-09-18 23:26:05 -0700

    hw1 Done

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,19 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+
+    return a.map{ |e| e.to_i + 2}.select {|e| e % 2 == 0}.uniq.select {|e| e <= 10}.sum
+    # a = a.select {|e| e % 2 == 0}
+    # a = a.uniq
+    # a = a.select {|e| e <= 10}
+
+
+    /
+    adds two to each number, keeps the even numbers only, 
+    removes any duplicates, rejects the resulting numbers 
+    greater than ten, 
+    and finally returns the sum of the resultingarray.
+    /
   end
 end
 
